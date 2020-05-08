5d6af78eb026d4ead3541043a5536ef3e90a5fc8
Igor <igor_barishpol@mail.ru>
2020-05-08 16:59:21 +0300

    Genres, people and countries added

---
+++ db/migrate/20200415200415_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.text :biography
+
+      t.timestamps
+    end
+  end
+end
